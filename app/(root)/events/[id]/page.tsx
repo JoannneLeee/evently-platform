bdfb0ff6cc974eb5214c678eb34bcc11c36d79b9
Shinnozaki <joanne.lee@binus.ac.id>
2024-08-18 17:40:27 +0700

    tes

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -21,7 +21,7 @@ const page = async ({ params: { id }, searchParams }: SearchParamProps) => {
 
   return (
     <>
-      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
+      <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain tes">
         <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl">
           <Image
             src={event.imageUrl}
